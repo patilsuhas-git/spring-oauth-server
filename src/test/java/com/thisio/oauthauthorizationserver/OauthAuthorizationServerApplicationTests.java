b4ca56b9de51cfd7156e92f4a24c1d03a5143fa7
Suhas Patil <suhas.patil@salesforce.com>
2022-09-19 10:03:42 -0700

    first commit

---
+++ src/test/java/com/thisio/oauthauthorizationserver/OauthAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thisio.oauthauthorizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthAuthorizationServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
