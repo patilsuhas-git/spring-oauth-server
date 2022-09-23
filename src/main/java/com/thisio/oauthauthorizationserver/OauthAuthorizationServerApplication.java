fc2ee5f6a4dab20d70d0733c557c42252338d166
Suhas Patil <suhas.patil@salesforce.com>
2022-09-23 11:19:25 -0700

    Added the code for security

--- src/main/java/com/thisio/oauthauthorizationserver/OauthAuthorizationServerApplication.java
+++ src/main/java/com/thisio/oauthauthorizationserver/OauthAuthorizationServerApplication.java
@@ -1,7 +1,16 @@
 package com.thisio.oauthauthorizationserver;
 
+import com.thisio.oauthauthorizationserver.domain.Role;
+import com.thisio.oauthauthorizationserver.domain.User;
+import com.thisio.oauthauthorizationserver.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class OauthauthorizationserverApplication {
@@ -10,4 +19,29 @@ public class OauthauthorizationserverApplication {
 		SpringApplication.run(OauthauthorizationserverApplication.class, args);
 	}
 
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	CommandLineRunner run(UserService userService) {
+		return args -> {
+			userService.saveRole(new Role(null, "ROLE_USER"));
+			userService.saveRole(new Role(null, "ROLE_MANAGER"));
+			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+			userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+			userService.saveUser(new User(null, "Suhas Patil", "suhas", "1234", new ArrayList<>()));
+			userService.saveUser(new User(null, "Shraddha Ghogare", "shraddha", "5678", new ArrayList<>()));
+			userService.saveUser(new User(null, "Arun Balchandran", "arun", "9012", new ArrayList<>()));
+
+			userService.addRoleToUser("suhas", "ROLE_SUPER_ADMIN");
+			userService.addRoleToUser("shraddha", "ROLE_MANAGER");
+			userService.addRoleToUser("shraddha", "ROLE_SUPER_ADMIN");
+			userService.addRoleToUser("arun", "ROLE_USER");
+			userService.addRoleToUser("arun", "ROLE_ADMIN");
+			userService.addRoleToUser("arun", "ROLE_ADMIN");
+		};
+	}
 }
---
+++ src/main/java/com/thisio/oauthauthorizationserver/security/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.thisio.oauthauthorizationserver.security;
+
+import com.thisio.oauthauthorizationserver.filter.CustomAuthenticationFilter;
+import com.thisio.oauthauthorizationserver.filter.CustomAuthorizationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+  private final UserDetailsService userDetailsService;
+  private final BCryptPasswordEncoder bCryptPasswordEncoder;
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+    customAuthenticationFilter.setFilterProcessesUrl("/api/login");
+    //cross site request forgery
+    http.csrf().disable();
+    http.sessionManagement().sessionCreationPolicy(STATELESS);
+    http.authorizeHttpRequests().antMatchers("/api/login/**").permitAll();
+    http.authorizeHttpRequests().antMatchers(GET, "/api/user/**").hasAnyAuthority("ROLE_USER");
+    http.authorizeHttpRequests().antMatchers(POST, "/api/user/save/**").hasAnyAuthority("ROLE_ADMIN");
+    http.authorizeHttpRequests().anyRequest().authenticated();
+    http.addFilter(customAuthenticationFilter);
+    http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+  }
+
+  @Bean
+  @Override
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+  }
+}
